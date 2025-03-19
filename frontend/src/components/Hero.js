0e76178c91e855006064f3a16ab055dfde49c5dd
Bob's Programming Academy <97769721+BobsProgrammingAcademy@users.noreply.github.com>
2025-03-19 21:24:50 +0100

    Code refactoring

--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -1,4 +1,5 @@
 import { LazyLoadImage } from 'react-lazy-load-image-component';
+// Material UI
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
