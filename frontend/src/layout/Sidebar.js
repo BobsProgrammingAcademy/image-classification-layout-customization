762d7405a83342561cc725515a6df60f49e695f5
Bob's Programming Academy <97769721+BobsProgrammingAcademy@users.noreply.github.com>
2025-03-22 15:38:55 +0100

    Code refactoring

--- frontend/src/layout/Sidebar.js
+++ frontend/src/layout/Sidebar.js
@@ -7,6 +7,8 @@ import Drawer from '@mui/material/Drawer';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { useTheme } from '@mui/material';
+
+// Material Icons
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
 import CameraAltIcon from '@mui/icons-material/CameraAlt';
 import InsertPhotoOutlinedIcon from '@mui/icons-material/InsertPhotoOutlined';
